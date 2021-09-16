bc353b61b975cdb239ce1b7c1feb52ec626458bf
kamogelo <kamogelosekati@gmail.com>
2021-09-16 15:00:50 +0200

    adding react native navigation app

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      < Button title="Go to About" onPress={() => navigation.navigate('AboutPage')} />
+    </View>
+  )
+}
+
+function AboutPage() {
+  return (
+    <View>
+      <Text>About Page</Text>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer> 
+     <Stack.Navigator initialRouteName="Home">
+       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="AboutPage" component={AboutPage} />
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
